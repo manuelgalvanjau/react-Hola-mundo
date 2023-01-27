38bbcb343cb84e1925a8e256945a9f8d349333e3
username <manuelsk150@gmail.com>
2023-01-27 11:54:50 -0600

    fixed package.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-const nombre = 'Sergio amigo de Pax';
+const nombre = 'Manuel G';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo, mañana vendré triste</h1>
+      <h1>Hola Mundo, mañana vendré sad</h1>
       <h2>Mi nombre es {nombre} </h2>
     </div>
   );
