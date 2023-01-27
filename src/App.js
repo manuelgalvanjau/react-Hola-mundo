11e2e34c32133f5f9bffec7a6f8642403fe0e6a6
username <manuelsk150@gmail.com>
2023-01-27 11:24:23 -0600

    All files upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
+const nombre = 'Sergio amigo de Pax';
+
 function App() {
   return (
     <div className="App">
-    <h1>Hola Mundo, mañana vendre</h1>
-    <h2>Mi nombre es {nombre}</h2>
+      <h1>Hola Mundo, mañana vendré triste</h1>
+      <h2>Mi nombre es {nombre} </h2>
     </div>
   );
 }
