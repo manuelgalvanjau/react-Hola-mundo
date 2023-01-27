c56709c06012ab791bf102407ddec96703b5e433
username <manuelsk150@gmail.com>
2023-01-27 10:43:49 -0600

    All files upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hola Mundo, mañana vendre</h1>
+    <h2>Mi nombre es {nombre}</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Hello from './App';
+import { NombreCompleto as Name } from './components/NombreCompleto';
+import Mascota from './components/Mascota';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Hello />
+    <Name name="Sergio" age={30} show={true} />
+    <Mascota name="Solovino" age = {8} show={true} />
+    <Name name="Pax" age={31} show={false} />
+    <Name name="Miriam" age={24} show={false} />
+  </React.StrictMode>
+);
